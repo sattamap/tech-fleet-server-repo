099b49df7060e3a2946ea9716e87dec9c952eb34
sattamap <sattamap@gmail.com>
2023-10-20 20:44:38 +0600

    added api for featured products

--- index.js
+++ index.js
@@ -35,13 +35,22 @@ async function run() {
 
       const productCollection = client.db("productDB").collection("product");
       const brandCollection = client.db("productDB").collection("brand");
+      const featuredCollection = client.db("productDB").collection("featured");
       const cartCollection = client.db("productDB").collection("cart");
       
+
+
       app.get('/brand', async(req,res)=>{
         const cursor = brandCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     })
+      app.get('/featured', async(req,res)=>{
+        const cursor = featuredCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+    
       app.get('/products', async(req,res)=>{
         const cursor = productCollection.find();
         const result = await cursor.toArray();
